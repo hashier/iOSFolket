76336a6d8de9147974aec86a842236deb8a6358a
Christopher Loessl <cloessl+github@gmail.com>
2015-12-09 08:22:40 +0100

    Adds: Some test code to play with XML

--- iOSFolket/ViewController.swift
+++ iOSFolket/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SWXMLHash
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,59 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        test()
+    }
+    
+    func test() {
 
+        let xmlWithNamespace = "<root xmlns:h=\"http://www.w3.org/TR/html4/\"" +
+            "  xmlns:f=\"http://www.w3schools.com/furniture\">" +
+            "  <h:table>" +
+            "    <h:tr>" +
+            "      <h:td>Apples</h:td>" +
+            "      <h:td>Bananas</h:td>" +
+            "    </h:tr>" +
+            "  </h:table>" +
+            "  <f:table>" +
+            "    <f:name>African Coffee Table</f:name>" +
+            "    <f:width>80</f:width>" +
+            "    <f:length>120</f:length>" +
+            "  </f:table>" +
+        "</root>"
+        
+        var xml = SWXMLHash.parse(xmlWithNamespace)
+        
+        // one root element
+        let count = xml["root"].all.count
+        
+        // "Apples"
+        print(xml["root"]["h:table"]["h:tr"]["h:td"][0].element!.text!)
+        
+        
+        // enumerate all child elements (procedurally)
+        func enumerate(indexer: XMLIndexer, level: Int) {
+            for child in indexer.children {
+                let name = child.element!.name
+                print("\(level) \(name)")
+                
+                enumerate(child, level: level + 1)
+            }
+        }
+        
+        enumerate(xml, level: 0)
+        
+        
+        // enumerate all child elements (functionally)
+        func reduceName(names: String, elem: XMLIndexer) -> String {
+            return names + elem.element!.name + elem.children.reduce(", ", combine: reduceName)
+        }
+        
+        print(xml.children.reduce("elements: ", combine: reduceName))
+    }
 
 }
 
