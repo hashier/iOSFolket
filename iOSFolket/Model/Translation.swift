09511e90e84400fb8fd135f22509f1d3cd6cac2a
Emmanuel Garnier <emmanuel.garnier@truecaller.com>
2015-12-11 10:27:28 +0100

    Added: First iteration of parsing

---
+++ iOSFolket/Model/Translation.swift
@@ -0,0 +1,15 @@
+//
+//  Translation.swift
+//  iOSFolket
+//
+//  Created by Emmanuel Garnier on 11/12/15.
+//  Copyright © 2015 iOSFolket. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Translation: Object {
+    dynamic var original = ""
+    dynamic var translation = ""
+}
---
+++ iOSFolket/Model/Word.swift
@@ -0,0 +1,19 @@
+//
+//  Word.swift
+//  iOSFolket
+//
+//  Created by Emmanuel Garnier on 10/12/15.
+//  Copyright © 2015 iOSFolket. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Word: Object {
+    dynamic var language = ""
+    dynamic var wordClass = ""
+    dynamic var value = ""
+    var translations = List<Translation>()
+    
+    
+}
